e16dc13c730d0efd61c38fba86a5384fb6ec8ef4
Erputra <enricoriski49@gmail.com>
2024-10-31 13:21:25 +0700

    pembuatan kontroller untuk handle semua data yang datang dari request

--- app/Http/Controllers/TransferDataController.php
+++ app/Http/Controllers/TransferDataController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class TransferDataController extends Controller
 {
-    //
+    use ApiResponse;
+
+    public function store(Request $request)
+    {
+        // Validate the incoming request
+        $request->validate([
+            '*.App_ID' => 'required|string|max:255',
+            '*.Server_ID' => 'required|string|max:255',
+            '*.Kode_Transaksi' => 'required|string|max:255',
+            '*.Tanggal' => 'required|date',
+            '*.Void' => 'boolean',
+            '*.Kode_Customer' => 'string|max:255|nullable',
+            '*.Nama_Customer' => 'string|max:255|nullable',
+            '*.Kode_Gudang' => 'string|max:255|nullable',
+            '*.Nama_Gudang' => 'string|max:255|nullable',
+            '*.Kode_Cabang' => 'string|max:255|nullable',
+            '*.Nama_Cabang' => 'string|max:255|nullable',
+            '*.Kode_Sales' => 'string|max:255|nullable',
+            '*.Nama_Sales' => 'string|max:255|nullable',
+            '*.Kode_Barang' => 'string|max:255|nullable',
+            '*.Nama_Barang' => 'string|max:255|nullable',
+            '*.Satuan' => 'string|max:50|nullable',
+            '*.Konversi' => 'numeric|nullable',
+            '*.Harga_Bruto' => 'numeric|nullable',
+            '*.Discount' => 'numeric|nullable',
+            '*.Harga_Netto' => 'numeric|nullable',
+            '*.DPP' => 'numeric|nullable',
+            '*.PPN' => 'numeric|nullable',
+            '*.Jumlah' => 'numeric|nullable',
+        ]);
+    
+        // Loop through each sale data and dispatch a job
+        foreach ($request->all() as $salesData) {
+            ProcessSales::dispatch($salesData);
+        }
+
+        return $this->successResponse([
+            'token' => '$token'], 'Sales records are being processed', 201);
+    }
 }
